4e1b415f4831eb1a2a87dee8d222a64738e066ec
sv176 <63410173+sv176@users.noreply.github.com>
2021-03-24 21:09:13 +0300

    Add files via upload

---
+++ Romi.ino
@@ -0,0 +1,124 @@
+#include "encoders.h"
+#include "PID.h"
+#include "kinematics.h"
+
+
+//Pin definitions for motor
+#define L_PWM_PIN 10
+#define L_DIR_PIN 16
+#define R_PWM_PIN  9
+#define R_DIR_PIN 15
+#define LINE_LEFT_PIN   A2 //Pin for the left line sensor
+#define LINE_CENTRE_PIN A3 //Pin for the centre line sensor
+#define LINE_RIGHT_PIN  A4 //Pin for the right line sensor
+#define BUZZER 6
+#define kpReturn 70
+#define kiReturn 0.0001
+#define kdReturn 1
+
+
+
+PID pidLeft( kpReturn, kiReturn, kdReturn );
+PID pidRight( kpReturn, kiReturn, kdReturn );
+
+Kinematics kinematics;
+unsigned long speed_update_ts;
+unsigned long demand_update_ts;
+float left_speed_loop;
+float countLeft_prev;
+float right_speed_loop;
+float countRight_prev;
+float demand;
+unsigned spd_update_ts;
+
+
+
+void setup() 
+{
+  // Initialise your other globals variables
+  // and devices.
+
+  setupEncoder0();
+  setupEncoder1();
+
+
+  speed_update_ts=millis();
+  demand= 3.0f;
+  demand_update_ts=millis();
+
+  countLeft_prev= countLeft;
+  countRight_prev= countRight;
+  // Initialise the Serial communication
+  Serial.begin(9600);
+  delay(1000);
+  Serial.println("***RESET***");
+}
+ 
+
+// Remember, loop runs again and again
+void loop(){
+  straightLine();
+}
+
+
+void straightLine() {
+  //Measure speed, verify
+
+  //- it looks sensible, direction
+  //- consider control relationship
+  unsigned long elapsed_time;
+  elapsed_time= millis()-speed_update_ts;
+  if(elapsed_time>40) {
+    speed_update_ts= millis();
+    
+    long left_diff;
+    long right_diff;
+    
+    left_diff= countLeft - countLeft_prev;
+    left_speed_loop= (float)left_diff;
+    right_diff= countRight - countRight_prev;
+    right_speed_loop= (float)right_diff;
+    
+    //velocity of left wheel in counts/s
+    left_speed_loop= left_speed_loop/elapsed_time;
+    right_speed_loop= right_speed_loop/elapsed_time;
+    //update last count
+    countLeft_prev = countLeft;
+    countRight_prev = countRight;
+  }
+  Serial.print(right_speed_loop);
+  Serial.print(",");
+  Serial.print(demand);
+  Serial.print(",");
+  Serial.print(left_speed_loop);
+  Serial.print("\n");
+  leftMotor(pidLeft.update(demand, left_speed_loop));
+  rightMotor(pidRight.update(demand, right_speed_loop));
+}
+
+// Defining motor functions
+void leftMotor(float speed) {
+  if (speed < -255.0f || speed > 255.0f) {
+    Serial.println("Invalid Left Motor Speed.");
+  }
+  else {
+    if (speed >= 0) digitalWrite( L_DIR_PIN, LOW );
+    else digitalWrite( L_DIR_PIN, HIGH );
+
+    speed = abs(speed);
+    analogWrite( L_PWM_PIN, speed );
+  }
+}
+
+void rightMotor(float speed) {
+  if (speed < -255.0f || speed > 255.0f) {
+    Serial.println("Motor speed is no.");
+  }
+  else {
+    if (speed >= 0) digitalWrite( R_DIR_PIN, LOW );
+    else digitalWrite( R_DIR_PIN, HIGH );
+
+    speed = abs(speed);
+    analogWrite( R_PWM_PIN, speed );
+  }
+}
