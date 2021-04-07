af34dfdec6dec5b10d86b61c8b2dd6b97abe8c5e
Sharlene <sharlenedsilva@gmail.com>
2021-04-07 14:33:57 +0100

    added beeps at initialization and emergency stop

--- Romi/Romi.ino
+++ Romi/Romi.ino
@@ -3,6 +3,7 @@
 #include "kinematics.h"
 #include <Wire.h>
 #include <VL6180X.h>
+
 //Pin definitions for motor
 #define L_PWM_PIN 10
 #define L_DIR_PIN 16
@@ -15,11 +16,13 @@
 #define kpReturn 70
 #define kiReturn 0.0001
 #define kdReturn 1
+
+// Proximity sensor variables
 VL6180X sensor;
 float light_meas;
 #define SCALING 2
 
-
+// Global variables
 PID pidLeft( kpReturn, kiReturn, kdReturn );
 PID pidRight( kpReturn, kiReturn, kdReturn );
 
@@ -35,10 +38,7 @@ unsigned spd_update_ts;
 
 
 
-void setup() 
-{
-  // Initialise your other globals variables
-  // and devices.
+void setup() {
   Serial.begin(9600);
   
   setupEncoder0();
@@ -56,7 +56,11 @@ void setup()
 
   countLeft_prev= countLeft;
   countRight_prev= countRight;
+  
   // Initialise the Serial communication
+
+  // Initilizing beeps
+  PlayBeep(4, 100); PlayBeep(4, 100); PlayBeep(4, 100);
 
   delay(1000);
   Serial.println("***RESET***");
@@ -66,19 +70,24 @@ void setup()
 // Remember, loop runs again and again
 void loop(){
   
-  light_meas= sensor.readAmbientSingle();
+  light_meas = sensor.readAmbientSingle();
   Serial.print(light_meas);
   Serial.print('\n');
+
   if(light_meas<1000) {
-  straightLine();
+    straightLine();
   }
   else{
+    // Play emergency stop beeps.
+    PlayBeep(4, 100); PlayBeep(4, 100);
+    
     leftMotor(0);
     rightMotor(0);
   }
 }
 
 
+// Function to move Romi in a straight line using PID
 void straightLine() {
   //Measure speed, verify
 
@@ -109,7 +118,8 @@ void straightLine() {
   rightMotor(pidRight.update(demand, right_speed_loop));
 }
 
-// Defining motor functions
+
+// Function to move the left motor
 void leftMotor(float speed) {
   if (speed < -255.0f || speed > 255.0f) {
     Serial.println("Invalid Left Motor Speed.");
@@ -123,6 +133,7 @@ void leftMotor(float speed) {
   }
 }
 
+// Function to move the right motor
 void rightMotor(float speed) {
   if (speed < -255.0f || speed > 255.0f) {
     Serial.println("Motor speed is no.");
@@ -134,4 +145,13 @@ void rightMotor(float speed) {
     speed = abs(speed);
     analogWrite( R_PWM_PIN, speed );
   }
+}
+
+
+// Function to play a beep once at given volume, for delay_ms long
+void PlayBeep(int volume, int delay_ms){
+  analogWrite(BUZZER, volume);
+  delay(delay_ms);
+  analogWrite(BUZZER, 0);
+  delay(delay_ms);
 }
